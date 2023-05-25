91968f6b18444cdefafe9c0382981c21785dd116
Your Name <your-email@example.com>
2023-05-25 14:44:09 +0530

    fix

--- public/assets/home_code.js
+++ public/assets/home_code.js
@@ -268,8 +268,7 @@ coinToPurchase.onclick = function (e) {
                 "image": "https://example.com/your_logo",
                 "order_id": data.order.id, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
                 "handler": function (response) {
-                    alert("SUCCESS");
-                   // updateCoinsInUserDataBase(userId, coinsUserWantToPurchase)
+                    updateCoinsInUserDataBase(userId, coinsUserWantToPurchase)
                 },
                 "prefill": { //We recommend using the prefill parameter to auto-fill customer's contact information especially their phone number
                     "name": "Gaurav Kumar", //your customer's name
@@ -305,6 +304,7 @@ coinToPurchase.onclick = function (e) {
 
 
 function updateCoinsInUserDataBase(userId, coinsUserPurchased, paymentReciept) {
+    alert("SUCCESS");
     fetch('http://vocaltwin.cloud/updateCoins', {
         method: 'POST',
         headers: {
