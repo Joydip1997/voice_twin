5e0c48baef2f94ad074c67f27e1411dadb185518
Your Name <your-email@example.com>
2023-05-25 14:40:00 +0530

    fix

--- public/assets/home_code.js
+++ public/assets/home_code.js
@@ -268,7 +268,8 @@ coinToPurchase.onclick = function (e) {
                 "image": "https://example.com/your_logo",
                 "order_id": data.order.id, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
                 "handler": function (response) {
-                    updateCoinsInUserDataBase(userId, coinsUserWantToPurchase)
+                    alert("SUCCESS");
+                   // updateCoinsInUserDataBase(userId, coinsUserWantToPurchase)
                 },
                 "prefill": { //We recommend using the prefill parameter to auto-fill customer's contact information especially their phone number
                     "name": "Gaurav Kumar", //your customer's name
